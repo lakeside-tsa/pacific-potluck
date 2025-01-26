965fc3f33d74e6e7196df079ec7511b0ed805670
Pixelated Ducky <sarahmroper@proton.me>
2025-01-26 00:54:17 -0500

    feat: add item pickups

--- pacific-duck/scripts/ducky.gd
+++ pacific-duck/scripts/ducky.gd
@@ -3,12 +3,21 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -320.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
+	# animations
+	if (velocity.x > 1 || velocity.x < -1):
+		animated_sprite_2d.animation = "run"
+	else:
+		animated_sprite_2d.animation = "idle"
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite_2d.animation = "jump"
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,6 +29,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 10)
 
 	move_and_slide()
+
+	var isleft = velocity.x < 0
+	animated_sprite_2d.flip_h = isleft
