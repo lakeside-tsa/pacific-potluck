b2653feb9829063bc6c851ba4c88dd336048b3f0
Pixelated Ducky <sarahmroper@proton.me>
2025-01-27 22:02:52 -0500

    feat: display ingredient counters

--- pacific-duck/scripts/ingredient_manager.gd
+++ pacific-duck/scripts/ingredient_manager.gd
@@ -6,15 +6,20 @@ var ing3 = 0
 
 @onready var ing_counter = $IngCounter
 
+func set_ing_label():
+	ing_counter.text = "ing1: " + str(ing1) + "\ning2: " + str(ing2) + "\ning3: " + str(ing3)
+
 func add_ing1():
 	ing1 += 1
 	print(ing1)
-
+	set_ing_label()
 
 func add_ing2():
 	ing2 += 1
 	print(ing2)
+	set_ing_label()
 
 func add_ing3():
 	ing3 += 1
 	print(ing3)
+	set_ing_label()
