232e32d1ded921a03b4ac47185bd5a13aedf1604
Pixelated Ducky <sarahmroper@proton.me>
2025-01-29 21:46:26 -0500

    feat: level sets ingredient names

--- pacific-duck/scripts/ingredient_manager.gd
+++ pacific-duck/scripts/ingredient_manager.gd
@@ -1,25 +1,37 @@
 extends Node
 
-var ing1 = 0
-var ing2 = 0
-var ing3 = 0
+var ing1_val = 0
+var ing2_val = 0
+var ing3_val = 0
+
+var ing1_name = "ing1"
+var ing2_name = "ing2"
+var ing3_name = "ing3"
+
 
 @onready var ing_counter = $IngCounter
 
 func set_ing_label():
-	ing_counter.text = "ing1: " + str(ing1) + "\ning2: " + str(ing2) + "\ning3: " + str(ing3)
+	ing_counter.text = ing1_name + ": " + str(ing1_val) + "\n" \
+					 + ing2_name + ": " + str(ing2_val) + "\n" \
+					 + ing3_name + ": " + str(ing3_val)
+
+func set_ing_names(name1, name2, name3):
+	ing1_name = name1
+	ing2_name = name2
+	ing3_name = name3
 
 func add_ing1():
-	ing1 += 1
-	print(ing1)
+	ing1_val += 1
+	print(ing1_val)
 	set_ing_label()
 
 func add_ing2():
-	ing2 += 1
-	print(ing2)
+	ing2_val += 1
+	print(ing2_val)
 	set_ing_label()
 
 func add_ing3():
-	ing3 += 1
-	print(ing3)
+	ing3_val += 1
+	print(ing3_val)
 	set_ing_label()
