141df7182f658c8fd456d2e4af4417e8be82b652
Pixelated Ducky <sarahmroper@proton.me>
2025-01-30 21:59:23 -0500

    feat: add spawning and some parallax

--- pacific-duck/scripts/ingredient.gd
+++ pacific-duck/scripts/ingredient.gd
@@ -6,9 +6,14 @@ enum IngredientNumber { ING_1 = 0, ING_2, ING_3 }
 @export var ingredient_number: IngredientNumber
 
 func _on_body_entered(_body: Node2D) -> void:
+	var collision_original = collision_mask
 	print( "ing " + str(ingredient_number + 1) + " desu")
 	ingredient_manager.add_ing(ingredient_number)
-	queue_free()
+	collision_mask = 0
+	hide()
+	await get_tree().create_timer(5.0).timeout
+	show()
+	collision_mask = collision_original
 
 func _on_body_exited(_body: Node2D) -> void:
 	pass
