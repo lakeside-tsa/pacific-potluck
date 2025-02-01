b34bda92e317ab46b55f95a29b1f6373224b592c
Pixelated Ducky <sarahmroper@proton.me>
2025-02-01 13:37:15 -0500

    feat: added p1 controls

--- pacific-duck/scripts/oscar.gd
+++ pacific-duck/scripts/oscar.gd
@@ -20,12 +20,12 @@ func _physics_process(delta: float) -> void:
 
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("p1_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("p1_left", "p1_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
