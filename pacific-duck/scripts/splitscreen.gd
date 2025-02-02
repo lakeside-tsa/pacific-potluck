5496550893f2559952c5949d9285afb4f1ac9da1
Pixelated Ducky <sarahmroper@proton.me>
2025-02-01 21:24:20 -0500

    fix: splitscreen wip

--- pacific-duck/scripts/splitscreen.gd
+++ pacific-duck/scripts/splitscreen.gd
@@ -7,30 +7,38 @@ var player1: Node
 var player2: Node
 
 func _ready():
-	var viewport1 = $GridContainer/SubViewportContainer1/SubViewport
-	var viewport2 = $GridContainer/SubViewportContainer2/SubViewport
+	var viewport1 = $HBoxContainer/SubViewportContainer1/SubViewport
+	var viewport2 = $HBoxContainer/SubViewportContainer2/SubViewport
 
 	viewport2.world_2d = viewport1.world_2d
 
-   # Instance and add GameManager
+	# Adjust viewport sizes
+	get_tree().root.connect("size_changed", Callable(self, "_on_window_resized"))
+	_on_window_resized()
+
 	var game_manager = preload("res://scenes/game_manager.tscn").instantiate()
 	add_child(game_manager)
 
 	game_manager.connect("level_loaded", Callable(self, "_on_level_loaded"))
 	game_manager.load_level(1)
+	print("Viewport1 size: ", $HBoxContainer/SubViewportContainer1/SubViewport.size)
+	print("Viewport2 size: ", $HBoxContainer/SubViewportContainer2/SubViewport.size)
 	print("ready done")
 
 func _on_level_loaded(level: Node, spawn_point1: Vector2, spawn_point2: Vector2):
-	var viewport1 = $GridContainer/SubViewportContainer1/SubViewport
-	var viewport2 = $GridContainer/SubViewportContainer2/SubViewport
-	
+	var viewport1 = $HBoxContainer/SubViewportContainer1/SubViewport
+	var viewport2 = $HBoxContainer/SubViewportContainer2/SubViewport
+	print("on_level_loaded started")
+
+
 	# Remove previous level if exists
 	if current_level:
 		current_level.queue_free()
 	
-	# Add new level to viewport1
 	current_level = level
 	viewport1.add_child(current_level)
+	var level_clone = current_level.duplicate()
+	viewport2.add_child(level_clone)
 	
 	# Instance and add players
 	player1 = preload("res://scenes/oscar.tscn").instantiate()
@@ -47,3 +55,11 @@ func _on_level_loaded(level: Node, spawn_point1: Vector2, spawn_point2: Vector2)
 	viewport2.get_node("Camera2").target = player2
 	
 	print("on_level_loaded done")
+
+func _on_window_resized():
+	var window_size = get_viewport().size
+	var viewport1 = $HBoxContainer/SubViewportContainer1/SubViewport
+	var viewport2 = $HBoxContainer/SubViewportContainer2/SubViewport
+	viewport1.size = Vector2(window_size.x / 2, window_size.y)
+	viewport2.size = Vector2(window_size.x / 2, window_size.y)
+	print("Viewport sizes updated: ", viewport1.size)
