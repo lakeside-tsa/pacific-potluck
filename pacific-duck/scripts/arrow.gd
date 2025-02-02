93964305f55f41f7583f4209ef2c1cdb65a47288
Pixelated Ducky <sarahmroper@proton.me>
2025-02-02 17:15:49 -0500

    chore: arrow wip

--- pacific-duck/scripts/arrow.gd
+++ pacific-duck/scripts/arrow.gd
@@ -1,12 +1,12 @@
 extends PathFollow2D
 
-var cooking_quality = {
+static var cooking_quality = {
 	"UNDER": 0.01,
 	"PERFECT": 0.33,
 	"OVER": 0.64
 }
 
-var speed = 0.2
+var speed = 0.17
 var target = 0.99
 var cooking_status = cooking_quality["UNDER"]
 
@@ -25,12 +25,15 @@ func cooking_movement(delta):
 		progress_ratio += delta * speed
 
 		if progress_ratio < cooking_quality["PERFECT"]:
-			print("cooking_status: UNDER")
+			pass
+			#print("cooking_status: UNDER")
 		elif progress_ratio > cooking_quality["UNDER"] and progress_ratio < cooking_quality["OVER"]:
-			print("cooking_status: PERFECT")
+			pass
+			#print("cooking_status: PERFECT")
 			#target = cooking_quality["PERFECT"]
 		elif progress_ratio >= cooking_quality["OVER"]:
-			print("cooking_status: OVER")
+			pass
+			#print("cooking_status: OVER")
 			#target = cooking_quality["OVER"]
 		else:
 			pass
