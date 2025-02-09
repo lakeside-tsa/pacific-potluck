7695d77421830556fc1ff40c903790ffec2f9717
Pixelated Ducky <sarahmroper@proton.me>
2025-02-09 16:14:08 -0500

    fix: rearrange game manager code

---
+++ scripts/game.gd
@@ -0,0 +1,66 @@
+extends Node
+
+@warning_ignore("unused_signal")
+signal level_loaded(level, spawn_point1, spawn_point2)
+
+@onready var viewport1 = $Splitscreen/CanvasLayer/HBoxContainer/SubViewportContainer1/SubViewport1
+@onready var viewport2 = $Splitscreen/CanvasLayer/HBoxContainer/SubViewportContainer2/SubViewport2
+@onready var subviewportcontainer2 = $Splitscreen/CanvasLayer/HBoxContainer/SubViewportContainer2
+
+@onready var current_level_number = 0
+@onready var current_level_node: Node
+@onready var player1: Node
+@onready var player2: Node
+
+var current_score = 0
+
+func _ready():
+	var level_scene = null
+	level_scene = load("res://scenes/level_0.tscn")
+	current_level_node = level_scene.instantiate()
+	player1 = preload("res://scenes/p3_duck.tscn").instantiate()
+	viewport1.add_child(current_level_node)
+	current_level_node.add_child(player1)
+	player1.hide()
+	subviewportcontainer2.hide()
+	get_viewport().size = DisplayServer.screen_get_size()
+
+
+func load_level(level_number: int):
+	current_level_number = level_number
+	current_score = 0
+	var level_scene = load("res://scenes/level_" + str(level_number) + ".tscn")
+
+	if current_level_node:
+		current_level_node.queue_free()
+
+	subviewportcontainer2.show()
+
+	current_level_node = level_scene.instantiate()
+	get_viewport().size = DisplayServer.screen_get_size()
+	viewport1.add_child(current_level_node)
+
+	player1 = preload("res://scenes/p1_oscar.tscn").instantiate()
+	player2 = preload("res://scenes/p2_abel.tscn").instantiate()
+	current_level_node.add_child(player1)
+	current_level_node.add_child(player2)
+	player1.global_position = current_level_node.get_node("SpawnPoint1").global_position
+	player2.global_position = current_level_node.get_node("SpawnPoint2").global_position
+
+	viewport2.world_2d = viewport1.world_2d
+	viewport1.get_node("Camera1").target = player1
+	viewport2.get_node("Camera2").target = player2
+
+
+func next_level():
+	load_level((current_level_number % 4) + 1)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("num01"):
+		load_level(1)
+	elif event.is_action_pressed("num02"):
+		load_level(2)
+	elif event.is_action_pressed("num03"):
+		load_level(3)
+	elif event.is_action_pressed("num04"):
+		load_level(4)
--- scripts/splitscreen.gd
+++ scripts/splitscreen.gd
@@ -1,69 +1,15 @@
 extends Node2D
 
-@onready var current_level: Node
-@onready var player1: Node
-@onready var player2: Node
 @onready var viewport1 = $CanvasLayer/HBoxContainer/SubViewportContainer1/SubViewport1
 @onready var viewport2 = $CanvasLayer/HBoxContainer/SubViewportContainer2/SubViewport2
-@onready var subviewportcontainer2 = $CanvasLayer/HBoxContainer/SubViewportContainer2
-@onready var game_manager = preload("res://scenes/game_manager.tscn").instantiate()
 
 
 func _ready():
-
-	var level_scene = null
-	viewport2.world_2d = viewport1.world_2d
-
 	get_tree().root.connect("size_changed", Callable(self, "_on_window_resized"))
 	_on_window_resized()
-
-	game_manager.name = "GameManager"
-	add_child(game_manager)
-	game_manager.connect("level_loaded", Callable(self, "_on_level_loaded"))
-
+	# this can probably be removed:
 	get_viewport().size = DisplayServer.screen_get_size()
 
-	level_scene = load("res://scenes/level_0.tscn")
-	current_level = level_scene.instantiate()
-	viewport1.add_child(current_level)
-
-	player1 = preload("res://scenes/p3_duck.tscn").instantiate()
-	current_level.add_child(player1)
-	viewport1.get_node("Camera1").target = player1
-	viewport2.get_node("Camera2").target = player1
-
-	player1.hide()
-	subviewportcontainer2.hide()
-
-func _on_level_loaded(level: Node, spawn_point1: Vector2, spawn_point2: Vector2):
-
-	if current_level:
-		current_level.queue_free()
-
-	current_level = level
-	viewport1.add_child(current_level)
-
-	player1 = preload("res://scenes/p1_oscar.tscn").instantiate()
-	player2 = preload("res://scenes/p2_abel.tscn").instantiate()
-	current_level.add_child(player1)
-	current_level.add_child(player2)
-
-	player1.global_position = spawn_point1
-	player2.global_position = spawn_point2
-
-	viewport1.get_node("Camera1").target = player1
-	viewport2.get_node("Camera2").target = player2
-	subviewportcontainer2.show()
 
 func _on_window_resized():
 	print("Viewport sizes updated: ", viewport1.size)
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("num01"):
-		game_manager.load_level(1)
-	elif event.is_action_pressed("num02"):
-		game_manager.load_level(2)
-	elif event.is_action_pressed("num03"):
-		game_manager.load_level(3)
-	elif event.is_action_pressed("num04"):
-		game_manager.load_level(4)
