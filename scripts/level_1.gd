2e8bea877b8ea6972e6dd47de1fdc273205945d2
Pixelated Ducky <sarahmroper@proton.me>
2025-02-09 01:32:11 -0500

    chore: update lvl 1

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -6,7 +6,3 @@ extends Node
 func _ready() -> void:
 	ingredient_manager.set_ing_names("Biscuits", "Seaweed", "Crabs")
 	Dialogic.start("res://dialog/level1.dtl")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
