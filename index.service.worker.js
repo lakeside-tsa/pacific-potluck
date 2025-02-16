216834aeca5b22f8d233187964836ccf6698fe83
pixelated-ducky <sarahmroper@proton.me>
2025-02-16 18:05:54 +0000

    Deploying to gh-pages from @ lakeside-tsa/pacific-potluck@dd01fb0656fdcfd673d84944fe260a199a9cee69 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739727720|17097899';
+const CACHE_VERSION = '1739729141|16712944';
 /** @type {string} */
 const CACHE_PREFIX = 'pacific-potluck-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
