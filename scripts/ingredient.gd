55fb415d9c89c060d238915a4a5259dc51cf8018
Pixelated Ducky <sarahmroper@proton.me>
2025-02-17 23:12:27 -0500

    feat: generic ingredients

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
 @onready var ingredient_manager = %IngredientManager
+@onready var sprite_texture = $Sprite2D
+@onready var collision_shape = $CollisionShape2D
 
 enum IngredientNumber { ING_1 = 0, ING_2, ING_3 }
 @export var ingredient_number: IngredientNumber
+
+func _ready():
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(_body: Node2D) -> void:
 	var collision_original = collision_mask
